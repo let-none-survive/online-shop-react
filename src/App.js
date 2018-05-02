3547a1bf8ff602ace32fd595ba450e7f2b2020d1
Roma Boyko <romaboyko2017@gmail.com>
2018-05-02 22:52:11 +0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Container } from 'semantic-ui-react'
+import {setBooks} from './actions/books';
+import axios from 'axios'
+import MenuComponent from './components/Menu'
+import BookCard from './components/BookCard'
+import { Card } from 'semantic-ui-react'
+
+class App extends Component {
+
+  componentWillMount() {
+    const { setBooks } = this.props;
+    axios.get('/books.json').then(({data}) => {
+      setBooks(data);
+    })
+  }
+
+  render() {
+    const { books, isReady } = this.props;
+    return (
+      <Container>
+      <MenuComponent />
+        <Card.Group itemsPerRow={4}>
+          {!isReady ? 'Loading...' : books.map(book => (<BookCard title={book.title} author={book.author} price={book.price} image={book.image  } />))}
+        </Card.Group>
+      </Container>
+  )}
+}
+
+const mapStateToProps = ({books}) => ({
+  books: books.items,
+  isReady: books.isReady
+});
+
+const mapDispatchToProps = dispatch => ({
+  setBooks: books => dispatch(setBooks(books))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
---
+++ src/components/Menu.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Menu } from 'semantic-ui-react'
+
+const MenuComponent = () => (
+  <Menu>
+    <Menu.Item name='browse' onClick={this.handleItemClick}>
+      Книжный магазин
+    </Menu.Item>
+
+    <Menu.Menu position='right'>
+      <Menu.Item name='signup' onClick={this.handleItemClick}>
+       Итого: <b>0</b>USD
+      </Menu.Item>
+
+      <Menu.Item name='help' onClick={this.handleItemClick}>
+       Корзина (<b>0</b>)
+      </Menu.Item>
+    </Menu.Menu>
+  </Menu>
+)
+export default MenuComponent
